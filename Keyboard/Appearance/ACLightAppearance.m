9c4790e1b274d6b7f34c062bdd410477a5d63ab4
Arnaud Coomans <arnaud.coomans@gmail.com>
2014-11-02 15:46:42 -0800

    Fixed all light appearance colors

--- Keyboard/Appearance/ACLightAppearance.m
+++ Keyboard/Appearance/ACLightAppearance.m
@@ -18,10 +18,9 @@
     static UIColor* _lightKeyColor;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPhone4,1"]) {
+        if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPad3,"]) {
             _lightKeyColor = [UIColor colorWithWhite:254/255.0 alpha:1.0];
-            
-        } else { // iPhone5,
+        } else {
             _lightKeyColor = [UIColor whiteColor];
         }
     });
@@ -32,10 +31,11 @@
     static UIColor* _lightKeyShadowColor;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPhone4,1"]) {
+        if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPad3,"]) {
+            _lightKeyShadowColor = [UIColor colorWithRed:142/255.0 green:145/255.0 blue:149/255.0 alpha:1.0];
+        } else if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPhone7,2"]) {
             _lightKeyShadowColor = [UIColor colorWithRed:139/255.0 green:142/255.0 blue:146/255.0 alpha:1.0];
-            
-        } else { // iPhone5,
+        } else {
             _lightKeyShadowColor = [UIColor colorWithRed:136/255.0 green:138/255.0 blue:142/255.0 alpha:1.0];
         }
     });
@@ -47,13 +47,11 @@
     static UIColor* _darkKeyColor;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPhone4,1"]) {
-            _darkKeyColor = [UIColor colorWithRed:183/255.0 green:191/255.0 blue:202/255.0 alpha:1.0];
-            
+        if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPad3,"]) {
+            _darkKeyColor = [UIColor colorWithRed:184/255.0 green:191/255.0 blue:202/255.0 alpha:1.0];
         } else if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPhone5,"]) {
             _darkKeyColor = [UIColor colorWithRed:174/255.0 green:179/255.0 blue:190/255.0 alpha:1.0];
-            
-        } else { // iPhone6,
+        } else {
             _darkKeyColor = [UIColor colorWithRed:172/255.0 green:179/255.0 blue:190/255.0 alpha:1.0];
         }
     });
@@ -68,14 +66,19 @@
     static UIColor* _darkKeyDisabledTitleColor;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPhone4,1"]) {
-            _darkKeyDisabledTitleColor = [UIColor colorWithRed:124/255.0 green:129/255.0 blue:137/255.0 alpha:1.0];
-            
-        } else if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPhone5,"]) {
+        if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPhone5,"]) {
             _darkKeyDisabledTitleColor = [UIColor colorWithRed:118/255.0 green:121/255.0 blue:129/255.0 alpha:1.0];
-            
-        } else { // iPhone6,
+        } else if (
+                   [[[UIDevice currentDevice] machine] hasPrefix:@"iPhone6,"] ||
+                   [[[UIDevice currentDevice] machine] hasPrefix:@"iPad4,"]
+                   ) {
             _darkKeyDisabledTitleColor = [UIColor colorWithRed:116/255.0 green:121/255.0 blue:129/255.0 alpha:1.0];
+        } else if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPhone7,2"]) {
+            _darkKeyDisabledTitleColor = [UIColor colorWithRed:116/255.0 green:121/255.0 blue:127/255.0 alpha:1.0];
+        } else if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPhone7,1"]) {
+            _darkKeyDisabledTitleColor = [UIColor colorWithRed:118/255.0 green:123/255.0 blue:130/255.0 alpha:1.0];
+        } else if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPad3,"]) {
+            _darkKeyDisabledTitleColor = [UIColor colorWithRed:125/255.0 green:129/255.0 blue:137/255.0 alpha:1.0];
         }
     });
     return _darkKeyDisabledTitleColor;
@@ -86,10 +89,9 @@
     static UIColor* _blueKeyColor;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPhone4,1"]) {
+        if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPad3,"]) {
             _blueKeyColor = [UIColor colorWithRed:9/255.0 green:126/255.0 blue:254/255.0 alpha:1.0];
-            
-        } else { // iPhone5,
+        } else {
             _blueKeyColor = [UIColor colorWithRed:0/255.0 green:122/255.0 blue:255/255.0 alpha:1.0];
         }
     });
@@ -100,13 +102,13 @@
     static UIColor* _blueKeyShadowColor;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPhone4,1"]) {
+        if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPhone5,"]) {
+            _blueKeyShadowColor = [UIColor colorWithRed:105/255.0 green:106/255.0 blue:109/255.0 alpha:1.0];            
+        } else if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPhone7,2"]) {
+            _blueKeyShadowColor = [UIColor colorWithRed:103/255.0 green:105/255.0 blue:108/255.0 alpha:1.0];
+        } else if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPad3,"]) {
             _blueKeyShadowColor = [UIColor colorWithRed:107/255.0 green:109/255.0 blue:112/255.0 alpha:1.0];
-            
-        } else if ([[[UIDevice currentDevice] machine] hasPrefix:@"iPhone5,"]) {
-            _blueKeyShadowColor = [UIColor colorWithRed:105/255.0 green:106/255.0 blue:109/255.0 alpha:1.0];
-            
-        } else { // iPhone6,
+        } else {
             _blueKeyShadowColor = [UIColor colorWithRed:104/255.0 green:106/255.0 blue:109/255.0 alpha:1.0];
         }
     });
